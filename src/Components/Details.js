d6032ed9356bf5554e7d947738fdcca5316d9c02
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-02-25 19:25:04 +0530

    k

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -7,7 +7,13 @@ export default function Details() {
     const [age,setAge]=useState();
     const [rollno,setRollno]=useState();
     const [searchv,Setsearchv]=useState();
+    const [updatev,Setupdatev]=useState();
+    const[up,setUp]=useState();
     const [result,setResult]=useState();
+    const [updateName,setUpdateName]=useState();
+    const [updateAge,setUpdateAge]=useState();
+    const [box,setBox]=useState();
+    const[flag,setFlag]=useState(0);
     const sub=(e)=>
     {
         data.details.push({n:name,ag:age,roll:rollno})
@@ -36,6 +42,49 @@ export default function Details() {
             setResult();
         }
     }
+   
+    const upd=()=>
+    {
+        console.log(updatev);
+        try{
+         var out=data.details.find((val)=>val.roll===updatev);
+         if(out===undefined)
+         {
+            setUp('');
+            setBox('');
+            setUpdateName(null);
+            setUpdateAge(null);
+            setFlag(0);
+         }
+         else
+         {
+          
+        setUp(<div>
+            <h6>What to Update</h6>
+            <h6><button onClick={()=>{setFlag(1);setBox(<h6 key="2">Name : <input type="text" value={updateName} onChange={(e)=>{setUpdateName(e.target.value);var obj=data.details.findIndex((o)=>o.roll===updatev);
+        data.details[obj].n=e.target.value;setData({...data});
+}}></input> </h6>);}}>
+                Name</button>
+            <button onClick={()=>{setFlag(1);setBox(<h6 key="1">Age : <input type="text" value={updateAge} onChange={(e)=>{setUpdateAge(e.target.value);var obj=data.details.findIndex((o)=>o.roll===updatev);
+        data.details[obj].ag=e.target.value;setData({...data});
+}}></input> </h6>);}}>
+                Age</button>
+            <br></br>
+            
+            </h6>
+        </div>)
+        }
+        }
+        catch(err)
+        {
+            setUp('');
+                        setFlag(0);
+
+            setBox('');
+            setUpdateName('');
+            setUpdateAge('');
+        }
+    }
     var rows=data.details.map((val,ind)=><tr><td>{val.n}</td><td>{val.ag}</td><td>{val.roll}</td></tr>);
   return (
   <>
@@ -59,7 +108,14 @@ export default function Details() {
      <label style={{marginRight:"10px"}}>Enter Roll No</label>
      <input type="text" value={searchv} onChange={(e)=>Setsearchv(e.target.value)}></input><button onClick={sear}>Search</button>
      {result}
+<br></br><br></br>
+     
+     <label style={{marginRight:"10px"}}>Enter Roll No</label>
+     <input type="text" value={updatev} onChange={(e)=>Setupdatev(e.target.value)}></input><button onClick={upd}>Search</button>
+     {up}
 
+               {box}
+               {flag===1?<button onClick={()=>{setBox('');setFlag(0)}}> Update </button>:<></>}
   </>
 )
 
